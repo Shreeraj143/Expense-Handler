e8d72218ce20358f5f38e33c1224b416313a3a3d
Shreeraj Shinde <shreerajs123@gmail.com>
2023-07-24 00:23:44 +0530

    Added Filtering and visual functionalities

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -11,6 +11,10 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -18,16 +22,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={changeFilterHandler}
         />
-        {props.items.map((expense) => {
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
